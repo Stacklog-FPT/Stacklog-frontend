500d0fb7b5ebb403cac3e1517267c767933ec1f1
Long Le <102498053+lelong2507@users.noreply.github.com>
2025-06-09 10:01:46 +0700

    feature(group chat inprogress)

--- src/components/ChatPageComponents/GroupChat/GroupChat.jsx
+++ src/components/ChatPageComponents/GroupChat/GroupChat.jsx
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
+import avatar from "../../../assets/ava-chat.png";
 import "./GroupChat.scss";
 const GroupChat = () => {
   const [groupChat, setGroupChat] = useState([
-    { id: 1, avatar: "", name: "", content: "", isRead: true },
-    { id: 2, avatar: "", name: "", content: "", isRead: true },
-    { id: 3, avatar: "", name: "", content: "", isRead: false },
-    { id: 4, avatar: "", name: "", content: "", isRead: false },
+    {
+      id: 1,
+      avatar: avatar,
+      name: "Maeve",
+      content: "Hello there, hangout?",
+      isRead: true,
+    },
+    {
+      id: 2,
+      avatar: avatar,
+      name: "Adam Groff",
+      content: "Oh hello, billard?",
+      isRead: true,
+    },
+    {
+      id: 3,
+      avatar: avatar,
+      name: "Eric",
+      content: "Hello, have time?",
+      isRead: false,
+    },
+    {
+      id: 4,
+      avatar: avatar,
+      name: "Otis Milburn",
+      content: "Opps, goodBye",
+      isRead: false,
+    },
   ]);
   return (
     <div className="group__chat__container">
@@ -13,9 +38,22 @@ const GroupChat = () => {
         <h2>Group chat</h2>
         <i className="fa-solid fa-arrow-down"></i>
       </div>
-      <div className="group__chat__list">{groupChat.map((item) => {
-        
-      })}</div>
+      <div className="group__chat__list">
+        {groupChat.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className={`group__chat__card${item.isRead ? " isRead" : ""}`}
+            >
+              <img src={item.avatar} alt="avatar" />
+              <div className="group__chat__card__content">
+                <h2>{item.name}</h2>
+                <p>{item.content}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };

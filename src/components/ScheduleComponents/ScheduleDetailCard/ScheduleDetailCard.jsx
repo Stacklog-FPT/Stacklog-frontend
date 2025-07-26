e497ae89853a637503175af95c0ed6a2a147e7b1
Long Le <102498053+lelong2507@users.noreply.github.com>
2025-07-26 19:55:16 +0700

    feature(create schedule detail card)

---
+++ src/components/ScheduleComponents/ScheduleDetailCard/ScheduleDetailCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ScheduleDetailCard.scss";
+const ScheduleDetailCard = () => {
+  return (
+    <div className="schedule__detail__card">
+      <div className="schedule__detail__card__container"></div>
+    </div>
+  );
+};
+
+export default ScheduleDetailCard;

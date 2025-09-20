9c824b828bc4a079a29e9a7e02b34642fe4dc487
Long Le <102498053+lelong2507@users.noreply.github.com>
2025-09-20 21:25:27 +0700

    feature(initial basic file for document)

---
+++ src/redux/slice/documentSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  documents: [],
+  document: {},
+  pending: false,
+  error: null,
+};
+
+export const documentSlice = createSlice({
+  name: 'document',
+  initialState: initialState,
+  reducers: {
+    setPending: (state, action) => {
+      state.pending = true;
+    },
+    setDocumentStart: (state, action) => {
+      state.documents = action.payload;
+    },
+    getDocumentDetail: (state, action) => {
+      state.document = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export default documentSlice.reducer();
---
+++ src/service/DocumentService.js
@@ -0,0 +1,7 @@
+export const getAllDocument = async (classId) => {};
+
+export const uploadDocument = async (classId, groupId) => {};
+
+export const getDocumentById = async (classId, groupId) => {};
+
+export const deleteDocumentById = async (classId, groupId, documentId) => {};

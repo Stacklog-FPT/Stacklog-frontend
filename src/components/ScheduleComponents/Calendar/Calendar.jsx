5e1c933ea01a526ec62bb0eb89070dc546d2e8e1
Thanhtc003 <162295561+Thanhtc003@users.noreply.github.com>
2025-12-17 19:16:37 +0700

    feat: Add custom messages and formats to Calendar component for improved event display

--- src/components/ScheduleComponents/Calendar/Calendar.jsx
+++ src/components/ScheduleComponents/Calendar/Calendar.jsx
@@ -261,6 +261,15 @@ export default function Calendar({ groupId, isPage }) {
           onEventDrop={moveEvent}
           resizable
           onEventResize={moveEvent}
+          views={['month', 'week', 'day']}
+          messages={{
+            today: 'Now',
+          }}
+          formats={{
+            eventTimeRangeFormat: ({ start }) => moment(start).format('h:mm A'),
+            timeGutterFormat: 'h:mm A',
+            agendaTimeRangeFormat: ({ start }) => moment(start).format('h:mm A'),
+          }}
         />
       </DndProvider>
 
